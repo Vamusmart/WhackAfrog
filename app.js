baaccad8d144a94117320fe086870a18e0c2a694
vamusmart <password>
2021-10-05 17:56:23 +0200

    Style Upgrade

--- app.js
+++ app.js
@@ -1,132 +1,107 @@
-
-
-//Clicking on the startGame button runs the game that show mrbump jumping randomly in a different hole.
-//Mrbump goes up in a different hole with an interval of 2 seconds (2000 milliseconds).
-//The setInterval calls the function "jumpHole"
-let counterMrbumpTopUp = 0;
-let int
-function startGame() {
-    int = setInterval(jumpHole, 1000);
-}
-
-//The function jumpHole due to the randomNumber returns a number between 1 and 6
-//that represents the hole where mrbump will turn up
-//if it's the #1 for example the elementById will show the picture of mrbump in the hole #1
-//and hide mrbump picture in all the other holes 
-//The getElementById("points") indicates the function where adds the points if the element has been clicked
-function jumpHole() {
-
-    let randomNumber = Math.floor(Math.random() * 6 + 1);
-    console.log(counterMrbumpTopUp);
-
-    //console.log(randomNumber);
-
-    if (randomNumber == 1) {
-        document.getElementById("mrbump1").style.visibility = "visible";
-        document.getElementById("mrbump1").src = "./mrbump.png";
-        document.getElementById("mrbump2").style.visibility = "hidden";
-        document.getElementById("mrbump3").style.visibility = "hidden";
-        document.getElementById("mrbump4").style.visibility = "hidden";
-        document.getElementById("mrbump5").style.visibility = "hidden";
-        document.getElementById("mrbump6").style.visibility = "hidden";
-        document.getElementById("points").innerHTML = points;
-        counterMrbumpTopUp++
-
-    } else if (randomNumber == 2) {
-        document.getElementById("mrbump2").style.visibility = "visible";
-        document.getElementById("mrbump2").src = "./mrbump.png";
-        document.getElementById("mrbump1").style.visibility = "hidden";
-        document.getElementById("mrbump3").style.visibility = "hidden";
-        document.getElementById("mrbump4").style.visibility = "hidden";
-        document.getElementById("mrbump5").style.visibility = "hidden";
-        document.getElementById("mrbump6").style.visibility = "hidden";
-        document.getElementById("points").innerHTML = points;
-        counterMrbumpTopUp++
-
-    } else if (randomNumber == 3) {
-        document.getElementById("mrbump3").style.visibility = "visible";
-        document.getElementById("mrbump3").src = "./mrbump.png";
-        document.getElementById("mrbump1").style.visibility = "hidden";
-        document.getElementById("mrbump2").style.visibility = "hidden";
-        document.getElementById("mrbump4").style.visibility = "hidden";
-        document.getElementById("mrbump5").style.visibility = "hidden";
-        document.getElementById("mrbump6").style.visibility = "hidden";
-        document.getElementById("points").innerHTML = points;
-        counterMrbumpTopUp++;
-
-    } else if (randomNumber == 4) {
-        document.getElementById("mrbump4").style.visibility = "visible";
-        document.getElementById("mrbump4").src = "./mrbump.png";
-        document.getElementById("mrbump1").style.visibility = "hidden";
-        document.getElementById("mrbump2").style.visibility = "hidden";
-        document.getElementById("mrbump3").style.visibility = "hidden";
-        document.getElementById("mrbump5").style.visibility = "hidden";
-        document.getElementById("mrbump6").style.visibility = "hidden";
-        document.getElementById("points").innerHTML = points;
-        counterMrbumpTopUp++;
-
-    } else if (randomNumber == 5) {
-        document.getElementById("mrbump5").style.visibility = "visible";
-        document.getElementById("mrbump5").src = "./mrbump.png";
-        document.getElementById("mrbump1").style.visibility = "hidden";
-        document.getElementById("mrbump2").style.visibility = "hidden";
-        document.getElementById("mrbump3").style.visibility = "hidden";
-        document.getElementById("mrbump4").style.visibility = "hidden";
-        document.getElementById("mrbump6").style.visibility = "hidden";
-        document.getElementById("points").innerHTML = points;
-        counterMrbumpTopUp++;
-
-    } else if (randomNumber == 6) {
-        document.getElementById("mrbump6").style.visibility = "visible";
-        document.getElementById("mrbump6").src = "./mrbump.png"
-        document.getElementById("mrbump1").style.visibility = "hidden";
-        document.getElementById("mrbump2").style.visibility = "hidden";
-        document.getElementById("mrbump3").style.visibility = "hidden";
-        document.getElementById("mrbump4").style.visibility = "hidden";
-        document.getElementById("mrbump5").style.visibility = "hidden";
-        document.getElementById("points").innerHTML = points;
-        counterMrbumpTopUp++;
-    }
-
-    //the if/else statement stop the game after 30 times mrbump is displayed
-    if (counterMrbumpTopUp === 20) {
-        document.body.style.background = "black url('./gameover.jpg') no-repeat top left";
-        //alert("You Losst!");
-        stopGame()
-    }
-
-    //if the player reaches the score of 10 the if/else statement display that he has won and stops the game.
-    if (points >= 5) {
-        document.body.style.background = "white url('./won3.gif') no-repeat top left";
-        stopGame()
-        points = 0;
-    }
-}
-
-//Points counter.
-let points = 0;
-
-//Clicking the picture stored on the "getElementById("mrbump1") it runs the function "score" 
-//that add one point each time you click on the mrbump image showed in the hole.
-
-document.getElementById("mrbump1").addEventListener("click", score);
-document.getElementById("mrbump2").addEventListener("click", score);
-document.getElementById("mrbump3").addEventListener("click", score);
-document.getElementById("mrbump4").addEventListener("click", score);
-document.getElementById("mrbump5").addEventListener("click", score);
-document.getElementById("mrbump6").addEventListener("click", score);
-
-function score() {
-    points += 1;
-}
-
-
-//Quit the game
-function stopGame() {
-    clearInterval(int);
-}
-
-//The function reload the page to start a new game
-function reload() {
-    location.reload();
-}
+window.addEventListener("load", () => {
+    document.querySelector("button").addEventListener("click", () => {
+      startGame();
+    });
+  });
+  
+  const grounds = document.querySelectorAll(".ground");
+  const length = grounds.length;
+  
+  var interval = setInterval(() => {
+    //Generate a random number
+    const random = Math.floor(Math.random() * length);
+  
+    //Remove the active class from every ground
+    grounds.forEach((e) => {
+      e.classList.remove("active");
+    });
+  
+    //Add the active class to random ground
+    grounds[random].classList.add("active");
+  }, 1000);
+  
+  const score = document.querySelector("#score > span");
+  let count = 0;
+  let click = 0;
+  
+  grounds.forEach((e) => {
+    e.addEventListener("click", () => {
+      //If ground has active class which means it has mole
+      //So increase the count
+      click++;
+  
+      if (e.classList.contains("active")) {
+        count++;
+        score.innerHTML = count;
+        console.log("Frogs caught: " + count);
+      }
+      if (count === 5) {
+        count = 0;
+        click = 0;
+        document.body.style.background =
+          "white url('./img/congblue.gif') no-repeat center";
+        document.body.style.backgroundSize = "contain";
+  
+        document.getElementsByTagName("main")[0].style.visibility = "hidden";
+      }
+      console.log("Mouse clicks: " + click);
+  
+      //the if/else statement stop the game after 5 times the frog is displayed
+      if (click === 15) {
+        stopGame();
+      }
+    });
+  });
+  
+  const startGame = () => {
+    const grounds = document.querySelectorAll(".ground");
+    const length = grounds.length;
+    const score = document.querySelector("#score > span");
+    let count = 0;
+    console.log(count);
+  
+    grounds.forEach((e) => {
+      e.addEventListener("click", () => {
+        //If ground has active class which means it has mole
+        //So increase the count
+        if (e.classList.contains("active")) {
+          count++;
+          console.log(count);
+          score.innerHTML = count;
+        }
+      });
+    });
+  
+    var interval = setInterval(() => {
+      //Generate a random number
+      const random = Math.floor(Math.random() * length);
+  
+      //Remove the active class from every ground
+      grounds.forEach((e) => {
+        e.classList.remove("active");
+      });
+  
+      //Add the active class to random ground
+      grounds[random].classList.add("active");
+    }, 1000);
+  };
+  
+  //Quit the game
+  const stopGame = () => {
+    clearInterval(interval);
+    count = 0;
+    // document.body.style.position = "absolute";
+    document.body.style.background =
+      "black url('./img/gameover.jpg') no-repeat center";
+    //   alert("Game Over");
+    click = 0;
+    document.getElementsByTagName("main")[0].style.visibility = "hidden";
+    document.body.style.backgroundSize = "contain";
+  
+    // document.getElementsByClassName("wrapper")[0].style.visibility = "hidden";
+  };
+  
+  function refreshPage() {
+    window.location.reload();
+  }
+  
